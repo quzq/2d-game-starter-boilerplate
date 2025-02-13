884ac1b02512b23e2b2f560d821bce289e295da3
quzq <user@MSI>
2025-02-13 23:02:18 +0900

    add loop

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 const main = (viewport: HTMLCanvasElement): void => {
   const ctx = viewport.getContext("2d");
   if (!ctx) return;
-  console.log("hello! developer world");
+
+  const loop = (time: number): void => {
+    //
+    requestAnimationFrame(loop);
+  };
+
+  requestAnimationFrame(loop);
 };
 main(document.getElementById("viewport") as HTMLCanvasElement);
